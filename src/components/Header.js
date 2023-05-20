ac68ba6e87a00060afdb08b2e9e1ce6d5ec36b07
Ngoc Nguyen <nnpk0410@gmail.com>
2023-05-19 22:32:28 -0500

    complete Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+// Make a Header component as a child of App. It should return:
+// a <header> element with the following elements inside:
+// an <h1> with the name of the blog, passed as a prop called name
+
+import React from "react";
+
+const Header = (props) => {
+    const {name} = props
+
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+    )
+}
+
+export default Header
