4c8faf20b7b06b5e954705b7402e44abdc972eea
Ngoc Nguyen <nnpk0410@gmail.com>
2023-05-19 23:34:21 -0500

    complete lab

---
+++ src/components/ArticleList.js
@@ -0,0 +1,25 @@
+// Make an ArticleList component as a child of App. It should return:
+
+// a <main> element with the following components inside:
+// an array of Article components (one component for each post passed down as a prop called post to ArticleList)
+// make sure to assign a unique key prop to each Article
+
+import React from "react";
+import Article from "./Article";
+
+const ArticleList = ({ posts }) => {
+  const postItems = posts.map((post) => {
+    return (
+      <Article
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+      />
+    );
+  });
+
+  return <main>{postItems}</main>;
+};
+
+export default ArticleList;
